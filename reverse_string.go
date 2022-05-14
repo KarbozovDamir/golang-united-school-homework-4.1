dca33467edce928b5f1f563321a5949cd741304c
KarbozovDamir <damirkap89@mail.ru>
2022-05-14 13:15:06 +0600

    reverse_string

--- reverse_string.go
+++ reverse_string.go
@@ -2,5 +2,15 @@ package reverse_string
 
 func ReverseString(input string) (output string) {
 	// solution goes here
+	// var output string
+	for i := len(input) - 1; i >= 0; i-- {
+		output += string(input[i])
+	}
 	return output
 }
+
+// func main() {
+// 	s := "Hello World!"
+// 	s = ReverseString(s)
+// 	fmt.Println(s)
+// }
